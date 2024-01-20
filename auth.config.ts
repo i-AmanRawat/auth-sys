c7dd965546c1c30237ab9878146c44ed9debfd9c
AmaNRwt01 <94544552+AmaNRwt01@users.noreply.github.com>
2024-01-20 20:29:40 +0530

    middleware and login done

---
+++ auth.config.ts
@@ -0,0 +1,36 @@
+import GitHub from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+
+import type { NextAuthConfig } from "next-auth";
+import { LoginSchema } from "./schemas";
+import { db } from "./lib/db";
+import { getUserByEmail } from "./data/user";
+
+export default {
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await getUserByEmail(email);
+
+          if (!user || !user.password) {
+            return null;
+          }
+
+          const passwordMatched = await bcrypt.compare(password, user.password);
+
+          if (passwordMatched) {
+            return user;
+          }
+
+          return null;
+        }
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
